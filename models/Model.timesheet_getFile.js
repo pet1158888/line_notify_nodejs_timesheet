3cdb4e34d43f5865f443774918ed11ec745f839e
pet1158888 <phaichayon.2011@gmail.com>
2021-11-12 17:33:35 +0700

    commit

--- models/Model.timesheet_getFile.js
+++ models/Model.timesheet_getFile.js
@@ -75,3 +75,28 @@ module.exports = Timesheet_getFile
 // จะส่งข้อมูลวันที่ 12 ตอน 16:00
 // ให้ไปค้นหาข้อมูล ของ วันที่ 11 ตอน 16:01 เป็นต้นไป
 //               จนถึงวันที่ 12 ตอน 16:00
+
+// SELECT 
+//     files.id,
+//     files.header_id,
+//     files.attach_file_name,
+//     files.attach_file,
+//     convert(varchar, files.created, 24) as create_time,
+//     convert(VARCHAR,files.created,103) as create_date,
+//     files.created,
+//     header.plan_id,
+//     header.machine_id,
+//     planning.jobid,
+//     planning.shift_id, 
+//    CASE
+//        WHEN planning.shift_id = 1 THEN 'กลางวัน'
+//        ELSE 'กลางคืน'
+//    END AS shift_name
+//    FROM mi.dbo.timesheet_attach_files files
+//    LEFT JOIN mi.dbo.timesheet_header header ON header.header_id = files.header_id
+//    LEFT JOIN mi.dbo.machine_planning planning ON planning.id = header.plan_id
+//    WHERE files.created > DateAdd(DAY, -1, GETDATE()) and files.created<=GETDATE()
+// 	 --WHERE files.created >= DATEADD(hh, -24, GETDATE())
+// 	 --WHERE files.created >= DATEADD(day, -1, GETDATE()) 
+//    --WHERE convert(DATE, files.created, 20) >= CONVERT(VARCHAR(10), GETDATE()-1, 23) + ' '  + '16:01:00' AND convert(DATE, files.created, 20) <= CONVERT(VARCHAR(10), GETDATE(), 23) + ' '  + '16:00:00'
+	 
