e8e3b442cd0c70fd65d47d2fc71175e063a4c63b
pet1158888 <phaichayon.2011@gmail.com>
2022-02-25 10:36:46 +0700

    add message Line No_data

--- models/Model.timesheet_getFile.js
+++ models/Model.timesheet_getFile.js
@@ -2,6 +2,8 @@ var LineNotify = require('../src/client');
 const mi_sequelize = require('../config/db.config');
 const ACCESS_TOKEN = "0sHlHdNIkCtt5gZR2jNR3L6mFTWG19orYbFM7lXPBx1";
 const ACCESS_TOKEN2 = "0ZQapzO7kMLFkz82AIJonnaDyVzQVTQMFVOA4XWA28d";
+//const ACCESS_TOKEN = "vtnF8VVYTh0Tye8iSMcRZwDgXS4w19qxZ6uq6GSryzm";
+//const ACCESS_TOKEN2 = "vtnF8VVYTh0Tye8iSMcRZwDgXS4w19qxZ6uq6GSryzm";
 //mi 0ZQapzO7kMLFkz82AIJonnaDyVzQVTQMFVOA4XWA28d
 //test Zz82ic2KrfKSuJA0ktg3hFq26oBHIvJIwsXGV2ykfv6
 //bot2 vtnF8VVYTh0Tye8iSMcRZwDgXS4w19qxZ6uq6GSryzm
@@ -13,9 +15,13 @@ const Timesheet_getFile = (timesheet) => {
     [];
 }
 const sendlineFN = (res) => {
-    res.status(200).json(timesheet_data)
+  res.status(200).json(timesheet_data)
     var interval = 5000; // how much time should the delay between two iterations be (in milliseconds)?
-    var promise = Promise.resolve();
+  var promise = Promise.resolve();
+  if (timesheet_data.length == 0) {
+    notify.sendMessage("วันนี้ไม่มีข้อมูลในระบบ")
+    notify2.sendMessage("วันนี้ไม่มีข้อมูลในระบบ")
+  }  
   timesheet_data.forEach(function (data) {
      promise = promise.then(function () {
        console.log("data", data);
@@ -26,7 +32,7 @@ const sendlineFN = (res) => {
         return new Promise(function (resolve) {
           setTimeout(resolve, interval);
         });
-  });
+     });
 });
   promise.then(function () {
     console.log('Loop finished.');
@@ -45,7 +51,7 @@ Timesheet_getFile.FunctionCkearVariable = () => {
 Timesheet_getFile.FunctionGetData = (req, res, next) => {
     Timesheet_getFile.FunctionCkearVariable();
     sql = " ";
-    sql += `SELECT 
+    sql += `SELECT TOP 1
     files.id,
     files.header_id,
     files.attach_file_name,
@@ -66,6 +72,7 @@ Timesheet_getFile.FunctionGetData = (req, res, next) => {
    LEFT JOIN mi.dbo.timesheet_header header ON header.header_id = files.header_id
    LEFT JOIN mi.dbo.machine_planning planning ON planning.id = header.plan_id
 	 LEFT JOIN mi.dbo.machine machine_tb ON machine_tb.machine_id = header.machine_id
+   --WHERE files.created = '2022-02-20'
    WHERE files.created > DateAdd(DAY, -1, GETDATE()) and files.created<=GETDATE()`;
     mi_sequelize.query(sql).then((data) => {
         timesheet_data.push(...data[0])
